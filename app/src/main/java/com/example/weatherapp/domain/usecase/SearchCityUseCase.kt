a0271977f762d2dd6b863c729eb6567e790cfcfd
Rakhim <younear20@gmail.com>
2026-01-02 16:39:32 +0300

    The basic structure of the project was created. Work with the API and the Internet has begun

---
+++ app/src/main/java/com/example/weatherapp/domain/usecase/SearchCityUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.domain.usecase
+
+import androidx.room.Query
+import com.example.weatherapp.domain.repository.SearchRepository
+import javax.inject.Inject
+
+class SearchCityUseCase @Inject constructor(
+    private val repository: SearchRepository
+){
+
+    suspend operator fun invoke(query: String) = repository.search(query)
+}
